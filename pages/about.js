b9ca70209572893f4392e93f454712f3b7d531b9
Mannex Pimentel <mannex247@gmail.com>
2020-06-06 19:42:13 -0400

    next js app

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About BitzCoin</h1>
+        </div>
+
+
+    </Layout>
+)
+
+
+export default About;
